b592ac44023572eb333d13f604d291368d4c5b6b
Ulisse Mini <ulisse.mini@gmail.com>
2023-10-14 13:11:10 -0400

    MVP website blocker based on title, using gpt-3.5

--- content.js
+++ content.js
@@ -1,51 +1,127 @@
+/// Code ordered from most important to least important
+
+async function shouldBlock() {
+  console.log(`------------ Checking ${document.title}`)
+  let settings = (await getStorage()).settings;
+  console.log('settings', settings)
+  if (!settings.instructions) return;
+  if (!settings.apiKey) return;
+
+  let instructions = settings.instructions
+
+  let date = new Date();
+  let time = date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: true });
+
+  const resp = await fetch('https://api.openai.com/v1/chat/completions', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${settings.apiKey}`,
+    },
+    body: JSON.stringify({
+      'model': 'gpt-3.5-turbo',
+      'messages': [
+        {'role': 'system', 'content': 'You are BlockGPT. an AI that blocks websites based on nuanced instructions and reasoning.'},
+        {'role': 'user', 'content': `Block instructions: ${instructions}`},
+        {'role': 'user', 'content': `Should the website with title "${document.title}" be blocked at ${time}? Call the function with the answer.`},
+      ],
+      'temperature': 0,
+      'functions': [{
+        'name': 'block',
+        'description': 'Pass true to block the site, or false not to',
+        'parameters': {
+          'type': 'object',
+          'properties': {
+            'reasoning': {
+              'type': 'string',
+              'description': 'Step-by-step reasoning for decision',
+            },
+            'block': {
+              'type': 'boolean',
+              'description': 'Whether to block the website',
+            }
+          },
+          'required': ['reasoning', 'block'],
+        },
+      }],
+    })
+  });
+  console.log(resp);
+  if (!resp.ok) {
+    console.log(`Error: ${resp.status} ${resp.statusText}`);
+    return;
+  }
+
+  const json = await resp.json();
+  message = json['choices'][0]['message']
+  let args = null;
+  if (message["function_call"] && message["function_call"]["name"] == 'block') {
+    try {
+      args = JSON.parse(message["function_call"]["arguments"])
+      console.log(`raw args: ${message['function_call']['arguments']}`)
+    } catch (e) {
+      console.log(`Error parsing arguments: ${e}`)
+    }
+  }
+
+  console.log(`--------- CONCLUSION: ${JSON.stringify(args)}`)
+  return args;
+}
+
+async function blockWebsite(reason) {
+    // Create a full-page overlay
+    const overlay = document.createElement('div');
+    overlay.style.position = 'fixed';
+    overlay.style.top = '0';
+    overlay.style.left = '0';
+    overlay.style.width = '100%';
+    overlay.style.height = '100%';
+    overlay.style.zIndex = '99999999'; // a high value to ensure it's on top
+    overlay.style.backgroundColor = 'rgba(255,255,255,0.95)';
+    overlay.style.display = 'flex';
+    overlay.style.justifyContent = 'center';
+    overlay.style.alignItems = 'center';
+    overlay.style.flexDirection = 'column';
+    overlay.style.fontSize = '24px';
+    overlay.style.color = '#333';
+
+    // Add block message and reason
+    overlay.innerHTML = `
+        <h1>This website is blocked</h1>
+        <p>${reason}</p>
+    `;
+
+    // Append the overlay to the body
+    document.body.replaceChildren(overlay);
+}
+
+
 async function main() {
-  if (await shouldBlock()) {
-    document.body.innerHTML = `
-  <h1 style="
-      font-family: 'Arial', sans-serif !important;
-      font-size: 24px !important;
-      color: black !important;
-      text-align: center !important;
-      margin: 20% auto !important;
-      padding: 20px !important;
-      background-color: white !important;
-      border: 2px solid red !important;
-      display: block !important;
-      width: 60% !important;
-      box-shadow: 0px 0px 10px rgba(0,0,0,0.5) !important;
-      max-width: 100% !important;
-      overflow: hidden !important;
-      word-wrap: break-word !important;
-  ">
-    This website is blocked based on its content
-  </h1>`;
+  const args = await shouldBlock();
+  if (args && args.block) {
+    blockWebsite(args.reasoning);
   }
 }
 
-function shouldBlock() {
+
+async function getStorage() {
   return new Promise((resolve, reject) => {
-    chrome.storage.sync.get('blockedPatterns', function(data) {
-      if (data.blockedPatterns) {
-        for (let pattern of data.blockedPatterns) {
-          let regex = new RegExp(pattern);
-          if (regex.test(document.title)) {
-            resolve(true);
-            return;
-          }
-        }
+    chrome.storage.sync.get(result => {
+      if (chrome.runtime.lastError) {
+        reject(new Error(chrome.runtime.lastError));
+      } else {
+        resolve(result);
       }
-      resolve(false);
     });
   });
 }
 
-
 const loaded = (document.readyState === 'complete' || document.readyState === 'interactive')
 
 if (loaded) {
-  console.log("Load already completed")
+  console.log('Load already completed')
   main()
 } else {
-  console.log("Waiting for load...")
+  console.log('Waiting for load...')
   document.addEventListener('DOMContentLoaded', main)
 }
--- options.js
+++ options.js
@@ -1,12 +1,24 @@
-document.getElementById('save').addEventListener('click', function() {
-  let patterns = document.getElementById('patterns').value.split('\n');
-  chrome.storage.sync.set({ "blockedPatterns": patterns });
+document.getElementById('settingsForm').addEventListener('submit', function(event) {
+  console.log("Submit")
+  event.preventDefault();
+  let formData = new FormData(this);
+  let settings = {};
+  for (const [key, value]  of formData.entries()) { settings[key] = value; }
+  console.log('saving settings', settings)
+  chrome.storage.sync.set({'settings': settings}, function() {
+    console.log("Settings saved")
+  });
 });
 
 // Load existing patterns on opening the options page
-chrome.storage.sync.get('blockedPatterns', function(data) {
-  if(data.blockedPatterns) {
-    document.getElementById('patterns').value = data.blockedPatterns.join('\n');
+chrome.storage.sync.get(function(data) {
+  if (data.settings) {
+    console.log("Past settings", data.settings)
+    const form = document.getElementById('settingsForm');
+    for (const [key, value] of Object.entries(data.settings)) {
+      const element = form.elements.namedItem(key);
+      if (element) { element.value = value; }
+    }
   }
 });
 
